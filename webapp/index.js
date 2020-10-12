b89610bb6ba81f466f349f770402003a0e8ae1fc
brandoncaulfield <19891236+brandoncaulfield@users.noreply.github.com>
2020-10-12 17:49:27 +0100

    Step 4 Two-Way Data Binding

--- webapp/index.js
+++ webapp/index.js
@@ -1,23 +1,26 @@
 sap.ui.require([
     "sap/m/Text",
-    "sap/ui/model/json/JSONModel"
-], function (Text, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/mvc/XMLView"
+], function (Text, JSONModel, XMLView) {
     "use strict"
 
     // Attach an anonymous function to the SAPUI5 'init' event
     sap.ui.getCore().attachInit(function () {
         // Create a JSON model from an object literal
         var oModel = new JSONModel({
-            greetingText: "Hi. my name is ... John"
+            firstName: "Harry",
+            lastName: "Hawk",
+            enabled: true,
+            panelHeaderText: "Data Biunding Basics"
         });
 
         // Assign the model object to the SAPUI5 core
         sap.ui.getCore().setModel(oModel);
         
-        // Display a text element whose text is derived
-        // from the model object
-        new Text({
-            text: "{/greetingText}"
+        // Display the XML view called "App"
+        new XMLView({
+            viewName: "sap.ui.demo.db.view.App"
         }).placeAt("content");
     })
 })
