910fe479f850602938af6da2801b450b0d834d87
brandoncaulfield <19891236+brandoncaulfield@users.noreply.github.com>
2020-10-15 14:38:42 +0100

    Step 12: Aggregation Binding Using Templates

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,7 +1,10 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/library"
-], function (Controller, mobileLibrary) {
+    "sap/m/library",
+    "sap/ui/core/Locale",
+    "sap/ui/core/LocaleData",
+    "sap/ui/model/type/Currency"
+], function (Controller, mobileLibrary, Locale, LocaleData, Currency) {
     "use strict"
 
     return Controller.extend("sap.ui.demo.db.controller.App", {
@@ -12,6 +15,13 @@ sap.ui.define([
                 oBundle.getText("mailSubject", [sFirstName]),
                 oBundle.getText("mailBody")
             );
+        },
+        formatStockValue: function(fUnitPrice, iStockLevel, sCurrCode) {
+            var sBrowserLocale = sap.ui.getCore().getConfiguration().getLanguage();
+            var oLocale = new Locale(sBrowserLocale);
+            var oLocaleData = new LocaleData(oLocale);
+            var oCurrency = new Currency(oLocaleData.mData.currencyFormat);
+            return oCurrency.formatValue([fUnitPrice * iStockLevel, sCurrCode], "string");
         }
     })
 })
--- webapp/index.js
+++ webapp/index.js
@@ -9,6 +9,11 @@ sap.ui.require([
 
     // Attach an anonymous function to the SAPUI5 'init' event
     sap.ui.getCore().attachInit(function () {
+        
+        var oProductModel = new JSONModel();
+        oProductModel.loadData("./model/products.json");
+        sap.ui.getCore().setModel(oProductModel, "products");
+        
         // Create a JSON model from an object literal
         var oModel = new JSONModel({
             firstName: "Harry",
